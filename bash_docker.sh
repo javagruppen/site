851bb1ca7174fd2a7f30ca3762a11a52e6ef1aad
Anders K. Andersen <aka@myself.com>
2019-11-13 08:46:01 +0100

    jekyll upgraded to version (3.8.6) from (3.8.5) + added clean command

--- bash_docker.sh
+++ bash_docker.sh
@@ -38,7 +38,7 @@
 # purpose:		The purpose of this script is to document how to make
 # 				jekyll builder run in a docker on your development workstation
 # ---------------------------------------------------------------------------
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $DIR
 
 # ---------------------------------------------------------------------------
@@ -47,48 +47,62 @@ cd $DIR
 # usage:
 #           bash_docker.sh run
 #
-function do_run {
-    docker run -p 4000:4000 -v $DIR:/site bretfisher/jekyll-serve
+function do_run() {
+  docker run -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve
 }
 
 # ---------------------------------------------------------------------------
-function do_stop {
-	echo "Stopping + removing container (will write error when container missing...)"
-	docker container stop javagruppen_jekyll
-	docker container rm  javagruppen_jekyll
+function do_stop() {
+  echo "Stopping + removing container (will write error when container missing...)"
+  docker container stop javagruppen_jekyll
+  docker container rm javagruppen_jekyll
 }
 
 # ---------------------------------------------------------------------------
-function do_start {
-	do_stop
-	echo "running jekyll on source in " $DIR
-	docker run --name javagruppen_jekyll \
-		--detach \
-		-p 4000:4000 \
-		-v $(pwd):/site \
-		bretfisher/jekyll-serve
+function do_start() {
+  do_stop
+  echo "running jekyll on source in " $DIR
+  docker run --name javagruppen_jekyll \
+    --detach \
+    -p 4000:4000 \
+    -v $(pwd):/site \
+    bretfisher/jekyll-serve
+  #
+}
+
+# ---------------------------------------------------------------------------
+# function:     clean
+# description:  jekyll server re-generate the ruby dependencies
+# background:   When major components are updated (in example jekyll it self)
+#               then dependencies probably also needs update
 #
+# purpose:      When jekyll will not run try to regenerate dependencies
+#               and check that helped.
+# usage:
+#           bash_docker.sh clean
+#
+function do_clean() {
+  rm -v Gemfile.lock
 }
 
 # ---------------------------------------------------------------------------
 
-function do_log {
-	echo "showing log for, ctrl C to stop " $DIR
-	docker container logs --follow javagruppen_jekyll
+function do_log() {
+  echo "showing log for, ctrl C to stop " $DIR
+  docker container logs --follow javagruppen_jekyll
 }
 
 # ---------------------------------------------------------------------------
-function do_open {
-	echo "on macos default browser will show resulting page"
-	open http://localhost:4000
+function do_open() {
+  echo "on macos default browser will show resulting page"
+  open http://localhost:4000
 }
 
 # execute command
 # ===============
 command=$1
 if [ -z "$command" ]; then
-command="run"
+  command="run"
 fi
 do_$command
 #
-
